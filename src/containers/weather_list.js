2449ccfe410424fb58446c35d9117477e37dae80
Quan Truong <qmtruong92@gmail.com>
2018-05-22 12:35:34 -0700

    Added units

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,46 +1,37 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 /**
  * Displays the weather list
  */
 class WeatherList extends Component {
 
-	constructor(props) {
-		super(props);
-		this.convertKelvinToFahrenheit = this.convertKelvinToFahrenheit.bind(this);
-	}
 
 	convertKelvinToFahrenheit(kelvins) {
 		console.log('Kelvins converted to Fahrenheit: ', ((kelvins - 273.15) * 1.8000) + 32.00);
 		return (((kelvins - 273.15) * 1.8000) + 32.00);
 	}
 	/**
-	 * Create table rows with the city name
+	 * Create table rows with the city name. Responsible for rendering
+	 * a single row. 
 	 * @param {*} cityData Weather data that is being passed in from
 	 * 						props
 	 */
 	renderWeather(cityData) {
 		const cityName = cityData.city.name; // city name from search query
-		const firstForecast = cityData.list[0]; // reference to first forecast
-		temperatureInKelvins = firstForecast.main.temp; // temperature from first forecast
-		const weatherInFahrenheit = () => {
-			convertKelvinToFahrenheit(temperatureInKelvins);
-		}
-		const weather = firstForecast.weather[0].main; // retrieve general weather conditions
-		const humidity = firstForecast.main.humidity;
+		const temps = cityData.list.map(weather => weather.main.temp); // kelvin units
+		const pressures = cityData.list.map(weather => weather.main.pressure); // measured in hectopascals
+		const humidities = cityData.list.map(weather => weather.main.humidity); 
+
 		return (
-			<tr>
-				<td key={cityName}>{cityName}</td>
-				<td key={cityName + 'Temperature'}>
-					{/* {this.convertKelvinToFahrenheit(temperatureInKelvins)} */}
-					{weatherInFahrenheit}
-				</td>
-				<td key={cityName + 'Weather'}>{weather}</td>
-				<td key={cityName + 'Humidity'}>{`${humidity}%`}</td>
+			<tr key={cityName}>
+				<td>{cityName} </td>
+				<td> <Chart data={temps} color="orange" units="K" /> </td>
+				<td> <Chart data={pressures} color="green" units="hPa" /> </td>
+				<td> <Chart data={humidities} color="orange" units="%" /> </td>
 			</tr>
 		);
 	}
-
 	
 	/**
 	 * Create a table that will hold the name of the City,
@@ -52,9 +43,9 @@ class WeatherList extends Component {
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temperaure</th>
-						<th>Weather</th>
-						<th>Humidity</th>
+						<th>Temperaure (K)</th>
+						<th>Pressure (hPa)</th>
+						<th>Humidity (%)</th>
 					</tr>
 				</thead>
 				<tbody>
